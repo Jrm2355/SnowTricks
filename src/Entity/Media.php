c551f1f9a66c0576c467080f50b4f1ace81b72fa
Jeremyon <jeremyon.jy@gmail.com>
2022-08-29 12:50:15 +0200

    réparation

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Entity;
@@ -65,71 +64,3 @@ class Media
         return $this;
     }
 }
-=======
-<?php
-
-namespace App\Entity;
-
-use App\Repository\MediaRepository;
-use Doctrine\ORM\Mapping as ORM;
-
-#[ORM\Entity(repositoryClass: MediaRepository::class)]
-class Media
-{
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column()]
-    private ?int $id = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $source = null;
-
-    #[ORM\ManyToOne(inversedBy: 'media')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Trick $trick = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $type = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getSource(): ?string
-    {
-        return $this->source;
-    }
-
-    public function setSource(string $source): self
-    {
-        $this->source = $source;
-
-        return $this;
-    }
-
-    public function getTrick(): ?Trick
-    {
-        return $this->trick;
-    }
-
-    public function setTrick(?Trick $trick): self
-    {
-        $this->trick = $trick;
-
-        return $this;
-    }
-
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
-
-    public function setType(string $type): self
-    {
-        $this->type = $type;
-
-        return $this;
-    }
-}
->>>>>>> main
